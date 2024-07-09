baf0e2658674894c16bcc2199f3b191758ca3b41
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-07-09 21:56:15 +0700

    fix wep menu

--- src/menu/weapon_menu.cpp
+++ src/menu/weapon_menu.cpp
@@ -5,31 +5,23 @@ namespace big
 	void weapon_menu::render_menu()
 	{
 		ImGui::BeginGroup();
-
 		ImGui::Checkbox("Infinite Frostcraft", &g_settings->weapon.inf_frostcraft);
-
 		ImGui::Checkbox("Max Sharpness", &g_settings->player.max_sharpness);
-
 		ImGui::Checkbox("Maxed Stat Set", &g_settings->player.maxed_stat_set);
-
 		ImGui::Checkbox("Maxed Armor Effect", &g_settings->player.maxed_armor_effect);
-		
 		ImGui::EndGroup();
 
-		ImGui::BeginGroup();
+		ImGui::SameLine(200.f);
 
+		ImGui::BeginGroup();
 		ImGui::Checkbox("Max Slot Armor", &g_settings->weapon.max_slot_armor);
-
 		ImGui::Checkbox("Inf Slinger Ammo", &g_settings->weapon.inf_slinger_ammo);
-
 		ImGui::EndGroup();
 
 		ImGui::BeginGroup();
-
 		ImGui::Checkbox("Frostcraft Recharge", &g_settings->weapon.frostcraft_recharge);
 		ImGui::InputFloat("Frostcraft Drawn", &g_settings->weapon.frostcraft_drawn, 1.f, 10.f);
 		ImGui::InputFloat("Frostcraft Sheathed", &g_settings->weapon.frostcraft_heat, 1.f, 10.f);
-
 		ImGui::EndGroup();
 
 		if (ImGui::CollapsingHeader("Weapon Buffs"))
